cfccd8e406e04bbea7b429fdc24c9ee9b203617d
kaio <kaioresendeoficial@outlook.com>
2023-07-28 17:32:54 -0300

    :tada: create project: AbyzShop API :tada:

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import admin from 'firebase-admin';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const serviceAccount = 'prodshopFirebaseAccess.json';
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+}
+bootstrap();
