28b14870e67e386eed7a21287d2e388f50f2806f
kaio <kaioresendeoficial@outlook.com>
2023-07-28 18:05:54 -0300

    :wrench: firebase connect :wrench:

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Welcome to AbyzShop!';
+  }
+
+  private readonly db: FirebaseFirestore.Firestore;
+
+  constructor() {
+    try {
+      this.db = admin.firestore();
+      console.log('Conexão com o Firebase estabelecida com sucesso!');
+    } catch (error) {
+      console.error('Erro ao conectar ao Firebase:', error);
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-//import admin from 'firebase-admin';
+import * as admin from 'firebase-admin';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  dotenv.config();
+
+  const serviceAccount = JSON.parse(process.env.FIRE_CONNECT);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+
   const app = await NestFactory.create(AppModule);
-
-  // const serviceAccount = './prodshopFirebaseAccess.json';
-
-  // admin.initializeApp({
-  //   credential: admin.credential.cert(serviceAccount),
-  // });
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
