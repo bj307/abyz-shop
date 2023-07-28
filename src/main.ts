1cbcc3124cd35f946788a0d87542b31696919f8d
kaio <kaioresendeoficial@outlook.com>
2023-07-28 17:43:07 -0300

    :wrench: vercel config :wrench:

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import admin from 'firebase-admin';
+//import admin from 'firebase-admin';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const serviceAccount = './prodshopFirebaseAccess.json';
+  // const serviceAccount = './prodshopFirebaseAccess.json';
 
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  });
+  // admin.initializeApp({
+  //   credential: admin.credential.cert(serviceAccount),
+  // });
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
